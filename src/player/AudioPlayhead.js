6e98f57829e8c340a8f948d6c30e0b089bef188d
Jamie Webber <jamiecwebber@gmail.com>
2019-08-06 21:11:47 -0400

    added inline style to playhead

--- src/player/AudioPlayhead.js
+++ src/player/AudioPlayhead.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import styles from './player.scss';
+
+
+const playheadStyle = {
+	border: '1px solid green',
+	width: '10vh',
+	height: '10vh'
+}
 
 class AudioPlayhead extends Component {
+	constructor(props) {
+		super(props);
+		this.canvas = React.createRef();
+	}
+
 	render() {
-		return <canvas id='playhead' />;
+		return <canvas style={playheadStyle} ref={this.canvas} />;
 	}
 }
 
