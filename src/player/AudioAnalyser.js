05945296882245de6d4d002b6112347c2449b8d7
Jamie Webber <jamiecwebber@gmail.com>
2019-08-06 21:16:16 -0400

    playhead visualizes the audio properly

--- src/player/AudioAnalyser.js
+++ src/player/AudioAnalyser.js
@@ -34,26 +34,6 @@ class AudioAnalyser extends Component {
 	}
 
 
-	draw() {
-		const canvas = this.canvas.current;
-		const height = canvas.height;
-		const width = canvas.width;
-		const context = canvas.getContext('2d');
-		let x = 0;
-		const sliceWidth = (width + 1.0)/ this.state.audioData.length;
-		context.lineWidth = 5;
-		context.strokeStyle = '#000000';
-		context.clearRect(0,0,width,height);
-		context.beginPath();
-		context.moveTo(0,height/2);
-		for (const item of this.state.audioData) {
-			const y = (item / 255.0) * height;
-			context.lineTo(x,y);
-			x += sliceWidth;
-		}
-		context.lineTo(x, height/2);
-		context.stroke();
-	}
 
 	componentWillUnmount() {
 		cancelAnimationFrame(this.rafId);
@@ -61,7 +41,7 @@ class AudioAnalyser extends Component {
 	}
 
 	render() {
-		return <AudioPlayhead />;
+		return <AudioPlayhead audioData={this.state.audioData} />;
 	}
 
 }
